7d0ee6e33955668d1f45ec6c00488534088d3271
Alone X <aravindyt001@gmail.com>
2022-03-12 16:53:51 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -8,42 +8,31 @@ Hey {}
 
 Welcome to {}
 Bot For help You to Create Session.
-[➼](https://telegra.ph/file/e1d35e61bd7e3bef4bfc8.jpg) So What U Waiting For Generat STRING Session
-───────────────────────
-
-If you don't trust this bot, 
-1) stop reading this message
-2) delete this chat
-───────────────────────
-Still reading?
+[➼](https://telegra.ph/file/9989afd6ad6ab59e1d43b.jpg) So What U Waiting For Generate String Session.
 You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
 
-✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: [Tᴇᴀᴍ DᴇCᴏᴅᴇ!](https://t.me/DeeCodeBots)
+Powered By [Zex](https://t.me/ZexNetwork)
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton("💟 Start Generating Session 💟", callback_data="generate")],
+        [InlineKeyboardButton("♨️ Start Generating String", callback_data="generate")],
     ]
 
     generate_button = [
-        [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+        [InlineKeyboardButton("♨️ Start Generating String", callback_data="generate")]
     ]
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("🎉 Start Generating Session 🎉", callback_data="generate")],
-        [InlineKeyboardButton("💕 Bot Status and More Bots 💕", url="https://t.me/DeeCodeBots/32")],
-        [
-            InlineKeyboardButton("How to Use 🤭❔", callback_data="help"),
-            InlineKeyboardButton("😏 About 😏", callback_data="about")
-        ],
-        [InlineKeyboardButton("🥱 More Amazing bots 🥱", url="https://t.me/DeeCodeBots")],
+        [InlineKeyboardButton("♨️ Start Generating String", callback_data="generate")],
+        [InlineKeyboardButton("🗯️ Help", callback_data="help"),],
+         [InlineKeyboardButton("💻 About", callback_data="about",],
     ]
 
     # Help Message
     HELP = """
-✨ **Available Commands** ✨
+💫 **Available Commands** 💫
 
 /about - About The Bot
 /help - This Message
