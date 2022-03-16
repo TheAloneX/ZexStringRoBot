62559953b519336b6898cc162c78efd015d82964
Alone X <aravindyt001@gmail.com>
2022-03-16 20:18:48 +0530

    Update must_join.py

--- ZexStringRoBot/must_join.py
+++ ZexStringRoBot/must_join.py
@@ -13,16 +13,16 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/ZexNetwork"
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"You must join [this channel](t.me/ZexNetwork) to use me. After joining try again !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton("✨ Join Channel ✨", url="t.me/ZexNetwork")]
                     ])
                 )
                 await msg.stop_propagation()
